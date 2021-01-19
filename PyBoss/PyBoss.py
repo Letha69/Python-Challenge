fd8b4e3b7faba4c3c9fa9f9eb723045e89905e8b
Letha Varughese <LV882769@WCUPA.EDU>
2021-01-18 21:44:08 -0500

    modify

--- PyBoss/PyBoss.py
+++ PyBoss/PyBoss.py
@@ -1,8 +1,13 @@
+# Import Dependencies
 import os
 import csv
 
-csvpath = os.path.join('PyBoss/Resources/employee_data.csv').replace("\\","/")
+#Input file path
+csvpath = os.path.join('Resources','employee_data.csv').replace("\\","/")
 
+#Declare variables
+
+name = []
 first_name= []
 last_name= []
 dob_ymd= []
@@ -13,11 +18,70 @@ state =[]
 state2 =[]
 emp_id =[]
 
+# Dictionary containing states names as keys and abbreviations as values (provided) 
+us_state_abbrev = {
+    'Alabama': 'AL',
+    'Alaska': 'AK',
+    'Arizona': 'AZ',
+    'Arkansas': 'AR',
+    'California': 'CA',
+    'Colorado': 'CO',
+    'Connecticut': 'CT',
+    'Delaware': 'DE',
+    'Florida': 'FL',
+    'Georgia': 'GA',
+    'Hawaii': 'HI',
+    'Idaho': 'ID',
+    'Illinois': 'IL',
+    'Indiana': 'IN',
+    'Iowa': 'IA',
+    'Kansas': 'KS',
+    'Kentucky': 'KY',
+    'Louisiana': 'LA',
+    'Maine': 'ME',
+    'Maryland': 'MD',
+    'Massachusetts': 'MA',
+    'Michigan': 'MI',
+    'Minnesota': 'MN',
+    'Mississippi': 'MS',
+    'Missouri': 'MO',
+    'Montana': 'MT',
+    'Nebraska': 'NE',
+    'Nevada': 'NV',
+    'New Hampshire': 'NH',
+    'New Jersey': 'NJ',
+    'New Mexico': 'NM',
+    'New York': 'NY',
+    'North Carolina': 'NC',
+    'North Dakota': 'ND',
+    'Ohio': 'OH',
+    'Oklahoma': 'OK',
+    'Oregon': 'OR',
+    'Pennsylvania': 'PA',
+    'Rhode Island': 'RI',
+    'South Carolina': 'SC',
+    'South Dakota': 'SD',
+    'Tennessee': 'TN',
+    'Texas': 'TX',
+    'Utah': 'UT',
+    'Vermont': 'VT',
+
+    'Virginia': 'VA',
+    'Washington': 'WA',
+    'West Virginia': 'WV',
+    'Wisconsin': 'WI',
+    'Wyoming': 'WY',
+}
 
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    csv_header = next(csvreader)
 
-for row in csvreader:    
-      #  The total number of months included in the dataset 
-    emp_id.append(row[0])
+    #skip the headerr
+    header = next(csvreader)
+
+
+  # Iterate through each row
+
+    for row in csvreader:    
+      #  Append employee id's into list
+      emp_id.append(row[0])
